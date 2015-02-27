4db9a0a0f999621ab10ab84f3156e6f4eaa56e65
Brian <b.bakar@gruenspar.de>
2015-02-27 12:01:13 +0100

    using list, not working built

--- regex.rb
+++ regex.rb
@@ -1,37 +1,49 @@
 ENV['SSL_CERT_FILE'] = File.expand_path('C:/Projects/Secutey/cacert.pem', __FILE__)
 
-require 'open-uri'
+def readFile
+	require 'open-uri'
 
-# Input URL
-puts("URL eingeben (Beispiel: https://www.example.de):\n")
-url = gets
-url = url.chomp
+	regex_list = [
+		/<script(.*)http:(.*)\.js/,
+		/<img (.*)http:(.*)/,
+	]
+	
+	# Input URL
+	puts("URL eingeben (Beispiel: https://www.example.de):\n")
+	url = gets
+	url = url.chomp
+	
+	check_result = true	
+	counter = 0
 
-check_result = true	
-counter = 0
+	# url = File.new("Quelltext.html", "r")
 
-# url = File.new("Quelltext.html", "r")
+	# open URL and get source code
+	# check if website opens resources with http://
+	open(url) do |source|
+		source.each_line do |line| 
+			counter = counter + 1
+			
+			for i in 0..regex_list.length
+				result = line.match(regex_list[i])
+			end
+			
+			unless result.nil?
+				check_result = false
+				puts counter
+				puts result
+			end
+		end	
+	end
 
-# open URL and get source code
-# check if website opens resources with http://
-open(url) do |source|
-	source.each_line do |line| 
-		counter = counter + 1
-		result = line.match(/<img (.*)http:(.*)/)
-		unless result.nil?
-			check_result = false
-			puts counter
-			puts result
-		end
-	end	
+	if check_result == false
+		# insecure
+		puts "Found. Line numbers above."	
+	else
+		# secure
+		puts "Not Found"
+	end
 end
 
-if check_result == false
-	# insecure
-	puts "Found. Line numbers above."	
-else
-	# secure
-	puts "Not Found"
-end
-
-# line.match(/<img (.*)http:(.*)/) or line.match(/<script(.*)http\:(.*)/) or line.match(/<link href(.*)http:(.*).css(.*)/)
+# line.match(/<img (.*)http:(.*)/) or line.match(/<script(.*)http:(.*)\.js/) or line.match(/<link href(.*)http:(.*).css(.*)/)
+puts readFile
