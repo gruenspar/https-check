69bec17bb626745e4a8b96b8d0c3b1ede952ab4c
janpapenbrock <lunoxx@gmail.com>
2015-03-04 17:34:45 +0100

    Move user input to binary.

--- lib/regex.rb
+++ lib/regex.rb
@@ -1,44 +1,44 @@
+require 'open-uri'
+
 ENV['SSL_CERT_FILE'] = File.expand_path('../cert/cacert.pem', File.dirname(__FILE__))
 
-def readFile
-	require 'open-uri'
+class HttpsCheck
 
-	regex_list = [
-		/<script(.*)http:(.*)\.js/,
-		/<img (.*)http:(.*)/,
-		/<link href(.*)http:(.*)\.css/
-	]
-	
-	# Input URL
-	puts("URL eingeben (Beispiel: https://www.example.de):\n")
-	url = gets
-	url = url.chomp
-	
-	check_result = true	
+    def self.check url
+        regex_list = [
+            /<script(.*)http:(.*)\.js/,
+            /<img (.*)http:(.*)/,
+            /<link href(.*)http:(.*)\.css/
+        ]
 
-	# open URL and get source code
-	# check if website opens resources with http://
-	open(url) do |source|
-		source.each_line.with_index do |line, index|
-		    line_number = index + 1
+        url = url.chomp
 
-			regex_list.each do |regex|
-				result = line.match regex
-				unless result.nil?
-					check_result = false
-					puts "L#{line_number}:  #{result}"
-				end
-			end
-		end	
-	end
+        check_result = true
 
-	if check_result == false
-		# insecure
-		puts "\nWARNING: #{url} contains insecure asset references."
-	else
-		# secure
-		puts "\nOK"
-	end	
-end	
+        # open URL and get source code
+        # check if website opens resources with http://
+        open(url) do |source|
+            source.each_line.with_index do |line, index|
+                line_number = index + 1
 
-puts readFile
+                regex_list.each do |regex|
+                    result = line.match regex
+                    unless result.nil?
+                        check_result = false
+                        puts "L#{line_number}:  #{result}"
+                    end
+                end
+            end
+        end
+
+        if check_result == false
+            # insecure
+            puts "\nWARNING: #{url} contains insecure asset references."
+        else
+            # secure
+            puts "\nOK"
+        end
+    end
+
+end
+
